682b0bd0b6f3158e196b93fbfbd7a5e56d05405d
suciutwo <andrewsuciu@gmail.com>
2014-11-11 12:01:53 -0800

    Print cocktail names.

--- embury.py
+++ embury.py
@@ -9,8 +9,10 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 @app.route('/')
 def index():
     allrecipes = recipe_data(None)
-    print len(allrecipes)
-    return render_template('index.jade', cocktailCount=len(allrecipes))
+    allnames = [item[0] for item in allrecipes.iterrows()]
+    return render_template('index.jade', cocktailCount=len(allrecipes),
+                           allnames=allnames)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
