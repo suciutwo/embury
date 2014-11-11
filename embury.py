ce28aab0f92e6837343d000a95247454ca44d83c
suciutwo <andrewsuciu@gmail.com>
2014-11-10 22:01:39 -0800

    Include cocktail python package.

--- embury.py
+++ embury.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from src.data_processing.matrix_generation import recipe_data
+
 
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
@@ -6,7 +8,9 @@ app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
 @app.route('/')
 def index():
-    return render_template('index.jade', youAreUsingJade=True)
+    allrecipes = recipe_data(None)
+    print len(allrecipes)
+    return render_template('index.jade', cocktailCount=len(allrecipes))
 
 if __name__ == '__main__':
     app.run(debug=True)
