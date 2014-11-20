c7685ea9d5ec2de624aedb068b3baa242259a096
suciutwo <andrewsuciu@gmail.com>
2014-11-19 17:30:42 -0800

    Integrate new searching interface from cocktails library.

--- embury.py
+++ embury.py
@@ -1,22 +1,20 @@
 import os
-import random
 from flask import Flask
 from flask import render_template
 from flask import jsonify
-from src.data_processing.matrix_generation import recipe_data
+from src.CocktailDirectory import CocktailDirectory
 
 
 app = Flask(__name__)
 app.config['DEBUG'] = os.environ.get('DEBUG', False)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
+c = CocktailDirectory()
 
 @app.route('/')
 def index():
-    all_recipes = recipe_data(None)
-    all_names = [item[0] for item in all_recipes.iterrows()]
-    all_ingredients = all_recipes.columns.values
-
+    all_ingredients = c.all_ingredients()
+    all_names = [name for name, recipe in c.search()]
     return render_template('index.jade',
                            ingredients=all_ingredients[5:10],
                            missingIngredients=all_ingredients[0:5],
@@ -28,24 +26,14 @@ def about():
     return render_template('about.jade')
 
 
-def random_drinks(n):
-    all_names = [(title, ingredients) for title, ingredients in recipe_data(None).iterrows()]
-    selected_drinks = random.sample(all_names, n)
-    processed_drinks = []
-    for name, ingredient_series in selected_drinks:
-        ingredients = []
-        ingredient_series.sort()
-        for ingredient, amount in ingredient_series.iteritems():
-            if amount > 0:
-                ingredient = ingredient.replace('_', ' ')
-                ingredients.append(ingredient)
-        processed_drinks.append({"name": name, "ingredients": ingredients})
-    return processed_drinks
-
-
 @app.route('/search/')
 def search():
-    return jsonify(drinks=random_drinks(5))
+    random_selection = c.random_drinks(5)
+    processed_drinks = []
+    for name, recipe in random_selection:
+        ingredients = [ingredient_name for ingredient_name, amount in recipe]
+        processed_drinks.append({"name": name, "ingredients": ingredients})
+    return jsonify(drinks=processed_drinks)
 
 
 @app.errorhandler(404)
